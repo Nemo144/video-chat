a049c97fffa72154a80e3e30dde6b47fe73ed663
Nnamdi Okeke <onnamdialex@gmail.com>
2022-07-29 21:59:29 +0100

    :sparkles: adding tailwind to styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { VideoPlayer } from "./component/VideoPlayer";
+import { Options } from "./component/Options";
+import { Notifications } from "./component/Notifications";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <div className="flex justify-center items-center border-y-8 bg-white">
+          Video Chat
+        </div>
+      </div>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
 };
 
 export default App;
