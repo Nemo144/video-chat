2c623cf40cd53117040015fd9293e91499b42acb
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-05 00:31:47 +0100

    :zap: Logic for answerCall function
    completed the logic necessary for the answerCall function that will be called on the answer button.

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useRef, useEffect } from "react";
 import { io } from "socket.io-client";
-import peer from "simple-peer";
+import Peer from "simple-peer";
 
 //to create the initial context
 const SocketContext = createContext();
@@ -11,8 +11,14 @@ const socket = io("http://localhost:5000");
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
+  const [me, setMe] = useState("");
+  const [call, setCall] = useState({});
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
 
   const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
 
   useEffect(() => {
     //to get permission to use user's device camera and audio
@@ -24,9 +30,36 @@ const ContextProvider = ({ children }) => {
         //to set the stream to my ref
         myVideo.current.srcObject = currentStream;
       });
-  });
 
-  const answerCall = () => {};
+    socket.on("me", (id) => setMe(id));
+
+    socket.on("calluser", ({ from, name: callerName, signal }) => {
+      setCall({ isReceivedCall: true, from, name: callerName, signal });
+    });
+  }, []);
+
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    //to create a peer capable of video call
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+
+    //using peer and socket to achieve the video connection
+    peer.on("signal", (data) => {
+      socket.emit("answercall", { signal: data, to: call.from });
+    });
+
+    //the video stream for the receiving user
+    peer.on("stream", (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    //connecting the call to socket.on("calluser"...) to get the entire info of the incoming call
+    peer.signal(call.signal);
+
+    //setting the video connectionRef to the peer
+    connectionRef.current = peer;
+  };
 
   const callUser = () => {};
 
