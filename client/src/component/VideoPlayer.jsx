1977114e8e588e14dc05c1daefe0992044fc0ce3
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-08 15:38:38 +0100

    :construction: video player component wip

--- client/src/component/VideoPlayer.jsx
+++ client/src/component/VideoPlayer.jsx
@@ -1,5 +1,13 @@
+import { Paper } from "@material-ui/core";
 import React from "react";
+import { SocketContext } from "../SocketContext";
 
 export const VideoPlayer = () => {
-  return <div>VideoPlayer</div>;
+  return (
+    <div className="flex flex-col justify-center ">
+      <Paper className="p-10 border-2 border-black border-solid m-10">
+        video
+      </Paper>
+    </div>
+  );
 };
