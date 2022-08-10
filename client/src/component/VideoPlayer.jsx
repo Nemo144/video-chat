46228fe53fac3b1300cb255859ba06b4b8c33d5c
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-10 14:36:29 +0100

    :construction: videoplayer component wip.
    myVideo is done;now to complete the other user's video

--- client/src/component/VideoPlayer.jsx
+++ client/src/component/VideoPlayer.jsx
@@ -1,25 +1,30 @@
-import { Paper } from "@material-ui/core";
-import React from "react";
+import React, { useContext } from "react";
 import { SocketContext } from "../SocketContext";
 
 export const VideoPlayer = () => {
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
+    useContext(SocketContext);
   return (
     <div className="flex flex-row justify-center ">
       {/* my video */}
-      <div className="p-20 border-2 border-black bg-white border-solid m-10">
-        <div className="w-full">
-          <div className="-my-16 -mx-16">Name</div>
-          <video playsInline muted ref={null} autoPlay className="" />
+      {stream && (
+        <div className="p-20 border-2  bg-transparent border-white border-solid m-10">
+          <div className="w-full">
+            <div className="-my-16 -mx-16">{name || "Name"}</div>
+            <video playsInline muted ref={myVideo} autoPlay className="" />
+          </div>
         </div>
-      </div>
+      )}
 
       {/* other person's video */}
-      <div className="p-20 border-2 border-black bg-white border-solid m-10">
-        <div className="w-full">
-          <div className="-my-16 -mx-16">Name</div>
-          <video playsInline ref={null} autoPlay className="" />
+      {callAccepted && !callEnded && (
+        <div className="p-20 border-2 border-black bg-white border-solid m-10">
+          <div className="w-full">
+            <div className="-my-16 -mx-16">{call.name || "Name"}</div>
+            <video playsInline ref={userVideo} autoPlay className="" />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
