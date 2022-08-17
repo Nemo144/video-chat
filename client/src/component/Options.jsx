3cb642eac54f03aaf9677871900c5c1841679f1f
Nnamdi Okeke <onnamdialex@gmail.com>
2022-08-17 18:00:57 +0100

    :rocket: Options component UI build

--- client/src/component/Options.jsx
+++ client/src/component/Options.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { SocketContext } from "../SocketContext";
 import { AiFillCopy } from "react-icons/ai";
+import { FiPhoneCall } from "react-icons/fi";
 
 export const Options = ({ children }) => {
   const [idToCall, setIdToCall] = useState("");
@@ -11,8 +12,8 @@ export const Options = ({ children }) => {
   return (
     <div className="w-96 mt-80 mb-0 p-0 sm:w-4/5  ">
       <div className="px-10">
-        <form className="" noValidate autoComplete="off">
-          <div className="flex flex-row bg-white w-full">
+        <form className="flex justify-center" noValidate autoComplete="off">
+          <div className="flex flex-row bg-white w-3/4 justify-between h-64">
             <div className="p-20">
               <h1>Account Info</h1>
               <input
@@ -29,7 +30,7 @@ export const Options = ({ children }) => {
                 className="bg-blue-600 border w-full p-2 flex flex-row justify-center items-center text-white mt-4 "
               >
                 <button type="button">
-                  <AiFillCopy className="mt-1" />
+                  <AiFillCopy className="mt-1 mr-1" />
                   copy your Id
                 </button>
               </CopyToClipboard>
@@ -38,8 +39,10 @@ export const Options = ({ children }) => {
             <div className="p-20">
               <h1>Make a call</h1>
               <input
+                className="mt-10 border-black border border-t-0 border-r-0 border-l-0"
                 type="text"
                 label="ID to call"
+                placeholder="Call"
                 value={idToCall}
                 onChange={(e) => setIdToCall(e.target.value)}
                 fullWidth
@@ -49,8 +52,9 @@ export const Options = ({ children }) => {
                   type="button"
                   color="#000"
                   onClick={leaveCall}
-                  className="m-20"
+                  className="bg-blue-600 border w-full p-2 flex flex-row justify-center items-center text-white mt-4 "
                 >
+                  <FiPhoneCall className="mr-1" />
                   Hang up
                 </button>
               ) : (
@@ -58,8 +62,9 @@ export const Options = ({ children }) => {
                   type="button"
                   color="#fff"
                   onClick={() => callUser(idToCall)}
-                  className="m-20"
+                  className="bg-blue-600 border w-full p-2 flex flex-row justify-center items-center text-white mt-4 "
                 >
+                  <FiPhoneCall className="mr-1" />
                   Call
                 </button>
               )}
